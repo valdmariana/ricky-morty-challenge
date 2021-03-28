9c7b207d7fd755cf6bda5a3255fe11e8f4e67d4d
Mariana Valdespino Ramos <marianavl96@gmail.com>
2021-03-28 16:26:29 -0400

    Se crean los servicios para listar locations y episodios

--- src/app/home/services/episodes/episodes.service.ts
+++ src/app/home/services/episodes/episodes.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { GlobalConstants } from '../../../shared/global-constants';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EpisodesService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getEpisodes(page: number): Observable<any> {
+    const params = new HttpParams().set('page', page.toString())
+    return this.http.get(GlobalConstants.apiURL + 'episode', { params });
+  }
+
 }
--- src/app/home/services/locations/locations.service.ts
+++ src/app/home/services/locations/locations.service.ts
@@ -1,9 +1,18 @@
+import { HttpParams, HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { GlobalConstants } from '../../../shared/global-constants';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LocationsService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getLocations(page: number): Observable<any> {
+    const params = new HttpParams().set('page', page.toString())
+    return this.http.get(GlobalConstants.apiURL + 'location', { params });
+  }
+
 }
