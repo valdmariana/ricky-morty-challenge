39a15403b21af899d3fa3f99a136b1292461f6ca
Mariana Valdespino Ramos <marianavl96@gmail.com>
2021-03-29 13:08:01 -0400

    Correccion en test

--- src/app/home/pages/episode-locations/episode-locations.component.spec.ts
+++ src/app/home/pages/episode-locations/episode-locations.component.spec.ts
@@ -1,16 +1,24 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { EpisodeLocationsComponent } from './episode-locations.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { EpisodesService } from '../../services/episodes/episodes.service';
+import { CharactersService } from '../../services/characters/characters.service';
 
 describe('EpisodeLocationsComponent', () => {
   let component: EpisodeLocationsComponent;
   let fixture: ComponentFixture<EpisodeLocationsComponent>;
 
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [HttpClientTestingModule],
+    providers: [EpisodesService, CharactersService]
+  }));
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ EpisodeLocationsComponent ]
+      declarations: [EpisodeLocationsComponent]
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
